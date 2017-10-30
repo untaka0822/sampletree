f321252c3f3044704442d7ba1ac24a60c44700db
untaka0822 <untaka0822@gmail.com>
2017-10-30 16:48:32 +0800

    初回コミット

---
+++ sampletree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampletree
+//
+//  Created by untaka0822 on 2017/10/30.
+//  Copyright © 2017年 untaka0822. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
